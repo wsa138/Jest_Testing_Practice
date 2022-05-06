d983bd237277c6424952eeb706426f1c0fab4262
William Addesso <wsaddesso@gmail.com>
2022-05-05 22:42:10 -0400

    Created findMin function in analyzeArray.js. Added findMin to analyzeArray fucntion.

--- code/analyzeArray.js
+++ code/analyzeArray.js
@@ -1,23 +1,34 @@
 //
 function analyzeArray(arr) {
   let analyzedArr = [];
-  let averageNum = average(arr);
-  let minNum;
+  let averageNum = findAverage(arr);
+  let minNum = findMin(arr);
   let maxNum;
   let arrLength;
-  return averageNum;
+  return `${averageNum}, ${minNum}`;
 
   // Find the mininum
   // Find the maximum
   // Find the length
 }
 
-// Find average
-function average(arr) {
+// Returns the average from array
+function findAverage(arr) {
   let total = arr.reduce((accumulator, currentValue) => {
     return accumulator + currentValue;
   }, 0);
   return total / arr.length;
+}
+
+// Returns minimum number from array
+function findMin(arr) {
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
 }
 
 export default analyzeArray;
